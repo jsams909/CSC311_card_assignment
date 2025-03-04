d62a7ba533bad026d66aad6bfcc55e088889d7fb
joshs <samsonknight909@gmail.com>
2025-03-03 20:11:20 -0500

    commit

--- src/main/java/com/example/csc311_card_assignment/HelloApplication.java
+++ src/main/java/com/example/csc311_card_assignment/HelloApplication.java
@@ -3,13 +3,17 @@ package com.example.csc311_card_assignment;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Random;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 327);
         stage.setTitle("Hello!");
--- src/main/java/com/example/csc311_card_assignment/HelloController.java
+++ src/main/java/com/example/csc311_card_assignment/HelloController.java
@@ -1,14 +1,46 @@
 package com.example.csc311_card_assignment;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
+import java.util.Objects;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+private Cards cards = new Cards();
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-}
+    private VBox Main_vbox;
+
+    @FXML
+    private Button drawButton;
+
+    @FXML
+    private ImageView firstCard;
+
+    @FXML
+    private ImageView fourthCard;
+
+    @FXML
+    private Button hintButton;
+
+    @FXML
+    private ImageView secondCard;
+
+    @FXML
+    private ImageView thirdCard;
+
+    @FXML
+    void drawCards(ActionEvent event) {
+
+        cards.generateCards();
+        firstCard.setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream("images/" + cards.deck.get(0)))));
+        secondCard.setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream("images/" + cards.deck.get(1)))));
+        thirdCard.setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream("images/" + cards.deck.get(2)))));
+        fourthCard.setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream("images/" + cards.deck.get(3)))));
+
+    }}
+
